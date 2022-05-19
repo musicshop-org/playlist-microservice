11ed28d7991c8cc679d815cf08cb46326d8b3c22
Benjamin Mayer <lolgame99@gmail.com>
2022-05-19 12:07:40 +0200

    added response + openapi definition

--- src/main/java/fhv/musicshop/RestController.java
+++ src/main/java/fhv/musicshop/RestController.java
@@ -2,14 +2,37 @@ package fhv.musicshop;
 
 import fhv.musicshop.domain.Playlist;
 import fhv.musicshop.domain.Song;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
 
-@Path("playlist-api")
-public class RestController {
+@OpenAPIDefinition(
+        info = @Info(
+                title = "OpenAPIDefinition",
+                description = "Playlist Microservice REST API",
+                version = "1.0.0"
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:9001/",
+                        description = "Playlist Microservice REST"
+                )
+        }
+)
+@Path("")
+public class RestController extends Application {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -20,21 +43,67 @@ public class RestController {
     @Transactional
     @GET
     @Path("/playlist/{ownerId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Playlist getPlaylistByOwnerId(@PathParam("ownerId") String ownerId) {
+    @APIResponses(
+            value = {
+                    @APIResponse(
+                            responseCode = "200",
+                            description = "Playlist found",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = Playlist.class)
+                                    )
+                            }
+                    ),
+                    @APIResponse(
+                            responseCode = "404",
+                            description = "Playlist not found",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.TEXT_PLAIN,
+                                            schema = @Schema(implementation = String.class)
+                                    )
+                            }
+                    )
+            })
+    public Response getPlaylistByOwnerId(@PathParam("ownerId") String ownerId) {
         PlaylistService playlistService = new PlaylistServiceImpl();
-        return playlistService.getPlaylistByOwnerId(ownerId).get();
+        Optional<Playlist> playlistOptional = playlistService.getPlaylistByOwnerId(ownerId);
+        if(playlistOptional.isEmpty()){
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity("Playlist not found")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        return Response
+                .status(Response.Status.OK)
+                .entity(playlistOptional.get())
+                .type(MediaType.APPLICATION_JSON)
+                .build();
     }
 
     @Transactional
     @POST
     @Path("/playlist/addSongs")
-    @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public boolean addSongsToPlaylist(List<Song> songs, @HeaderParam("ownerId") String ownerId){
+    @APIResponse(
+            responseCode = "200",
+            description = "Songs added to Playlist",
+            content = {
+                    @Content(
+                            mediaType = MediaType.TEXT_PLAIN,
+                            schema = @Schema()
+                    )
+            }
+    )
+    public Response addSongsToPlaylist(List<Song> songs, @HeaderParam("ownerId") String ownerId){
         PlaylistService playlistService = new PlaylistServiceImpl();
         playlistService.addSongsToPlaylist(ownerId, songs);
-        return true;
+        return Response
+                .status(Response.Status.OK)
+                .type(MediaType.TEXT_PLAIN)
+                .build();
     }
 
 }
