7eb23ce75698943a28cd7d882043bb09ec4706cd
Benjamin Mayer <lolgame99@gmail.com>
2022-05-28 12:30:58 +0200

    changed port & removed tests

--- src/test/java/fhv/musicshop/RestControllerIT.java
+++
@@ -1,9 +0,0 @@
-package fhv.musicshop;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class RestControllerIT extends RestControllerTest {
-
-    // Execute the same tests but in native mode.
-}
